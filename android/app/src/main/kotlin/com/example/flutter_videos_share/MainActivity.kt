da4a8308e2f5ac1952a1d0925cf4ed5237ad35c5
Nib1sin <38408560+Nib1sin@users.noreply.github.com>
2024-06-05 20:40:45 +0200

    INIT - App para compartir videos

---
+++ android/app/src/main/kotlin/com/example/flutter_videos_share/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_videos_share
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyVideoShareApp());
+}
+
+class MyVideoShareApp extends StatelessWidget {
+  const MyVideoShareApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
